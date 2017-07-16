049d86bb726bc8ef4a58e878bd31c0713a1a4f13
Bhalchandra <bhalchandra@BhalchadrasMBP2.fritz.box>
2017-07-16 19:54:12 +0200

    Added empty classes and interfaces

---
+++ SmaccIt/src/main/java/org/smacc/smaccit/controllers/EmailServiceControllers.java
@@ -0,0 +1,15 @@
+package org.smacc.smaccit.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Controller class to get access API for sendng mails. 'Thymeleaf' template
+ * engine has been used to render the front end.
+ * 
+ * @author Bhalchandra
+ *
+ */
+@Controller
+public class EmailServiceControllers {
+
+}
