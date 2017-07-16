8197e20e14caff0c4236b9107b5c387cdadbfa54
Bhalchandra <bhalchandra@BhalchadrasMBP2.fritz.box>
2017-07-16 20:21:08 +0200

    Thymleaf template and dependecies added

--- SmaccIt/src/main/java/org/smacc/smaccit/domain/EmailModel.java
+++ SmaccIt/src/main/java/org/smacc/smaccit/domain/EmailModel.java
@@ -1,5 +1,58 @@
 package org.smacc.smaccit.domain;
 
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Email;
+
+/**
+ * This is model class for Email Entity
+ * 
+ * @author Bhalchandra
+ *
+ */
 public class EmailModel {
 
+	@NotNull
+	@Email
+	private String sendTo;
+
+	@NotNull
+	@Email
+	private String from;
+
+	private String emailBody;
+
+	private String subject;
+
+	public String getSendTo() {
+		return sendTo;
+	}
+
+	public void setSendTo(String sendTo) {
+		this.sendTo = sendTo;
+	}
+
+	public String getEmailBody() {
+		return emailBody;
+	}
+
+	public void setEmailBody(String emailBody) {
+		this.emailBody = emailBody;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
 }
